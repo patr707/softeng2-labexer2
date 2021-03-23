8fe4fd89afa6bdd05f4d9cff3c3368f909066c4e
Patty <patricialao72@gmail.com>
2021-03-24 00:07:59 +0800

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold (
+      home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text ("My First Application in Flutter"),
+        title: Text("My First Application in Flutter"),
         centerTitle: true,
         backgroundColor: Colors.pink[200],
       ),
       body: Center(
-        child: Text(
-            "Patricia Iris F. Lao",
-          style: TextStyle(color: Colors.pink[300], fontSize: 25),
-        )
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Image.asset('image/ml.jpg'),
+                SizedBox(height:30),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                  ),
+                ),
+
+              ]
+            )
+
+          )
       ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            // Add your onPressed code here!
-          },
-          label: const Text('Okay'),
-          icon: const Icon(Icons.thumb_up),
-          backgroundColor: Colors.pink,
-        ),
-    ),
-  ));
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        label: const Text('Okay'),
+        icon: const Icon(Icons.thumb_up),
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
 }
+
+
 
